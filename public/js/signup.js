d608dcd477d16c1a9b710a303c9af50c070f1184
RaihanAkter03 <raihanakter26@gmail.com>
2020-12-26 09:54:52 -0600

    worked on signup js file and change the button mae from submit to sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,37 +2,46 @@ const $ = window.$;
 
 $(document).ready(() => {
   // Getting references to our form and input
-  const signUpForm = $('form.signup');
-  const emailInput = $('input#email-input');
-  const passwordInput = $('input#password-input');
+  const signupButton = $('#signup');
+  const firstName = $('#firstname');
+  const lastName = $('#lastname');
+  const email = $('#email');
+  const password = $('#password');
 
-  // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on('submit', event => {
+  // When the signup button is clicked, we validate the email and password, firstname , lastname are not blank
+  signupButton.on('submit', event => {
     event.preventDefault();
     const userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      firstName: firstName.val().trim(),
+      lastName: lastName.val().trim(),
+      email: email.val().trim(),
+      password: password.val().trim()
     };
 
     if (!userData.email || !userData.password) {
       return;
     }
-    // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
-    emailInput.val('');
-    passwordInput.val('');
+    // If we have an email and password , firstname and lastname, run the signUpUser function
+    signUpUser(userData.email, userData.password,userData.firstName,userData.lastName);
+    email.val('');
+    password.val('');
+    firstName.val('');
+    lastName.val('');
+
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser (email, password) {
+  function signUpUser (email, password,firstName,lastName) {
     $.post('/api/signup', {
       email: email,
-      password: password
+      password: password,
+      firstName: firstName,
+      lastName: lastName
     })
       .then(() => {
         window.location.replace('/members');
-        // If there's an error, handle it by throwing up a bootstrap alert
+        // If there's an error, handle it by throwing up a alert
       })
       .catch(handleLoginErr);
   }
