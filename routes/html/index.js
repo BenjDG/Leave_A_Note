2a98a580c8f95eb07ae669037f7441d4a3ea1d47
jhuynh24 <jhuynh1735@gmail.com>
2020-12-29 10:51:25 -0600

    updated html routes to render a view

--- routes/html/index.js
+++ routes/html/index.js
@@ -9,8 +9,8 @@ router.get('/', (req, res) => {
   if (req.user) {
     res.redirect('/members');
   }
-
-  res.sendFile(path.join(__dirname, '../../public/signup.html'));
+res.render("signup");
+  //res.sendFile(path.join(__dirname, '../../public/signup.html'));
 });
 
 router.get('/login', (req, res) => {
@@ -18,8 +18,8 @@ router.get('/login', (req, res) => {
   if (req.user) {
     res.redirect('/members');
   }
-
-  res.sendFile(path.join(__dirname, '../../public/login.html'));
+res.render("login");
+  //res.sendFile(path.join(__dirname, '../../public/login.html'));
 });
 
 // Route for logging user out
@@ -31,7 +31,8 @@ router.get('/logout', (req, res) => {
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 router.get('/members', isAuthenticated, (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/members.html'));
+  //res.sendFile(path.join(__dirname, '../../public/members.html'));
+  res.render("members");
 });
 
 module.exports = router;
