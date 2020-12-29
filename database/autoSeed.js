c3babd8f7bf6f1e2ecaf7453596318b8831b17fb
Ben Galloway <bdgalloway85@gmail.com>
2020-12-29 01:35:39 -0600

    fixed first name and last name to db & added an auto seed on start

---
+++ database/autoSeed.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const db = require('../models')
+
+module.exports = async function seed() {
+	const jane = await db.User.create({
+		first_name: "Jane",
+        last_name: "Doe",
+        email: "1@2.com",
+        password: process.env.JANE_PASSWORD
+
+	});
+	console.log("Jane's auto-generated ID:", jane.id);
+}
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const morgan = require('morgan');
 // Requiring passport as we've configured it
 const passport = require('./config/passport');
 const routes = require('./routes');
+const seed = require('./database/autoSeed')
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -51,7 +52,9 @@ app.use(morgan('dev'));
 app.use(routes);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync(SYNC_OPTIONS).then(() => {
+db.sequelize.sync(SYNC_OPTIONS)
+.then(() => seed())
+.then(() => {
   app.listen(PORT, () => {
     console.log(
       '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
