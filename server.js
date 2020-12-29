0606a69b4b9a734e8f32974769429445f4a4bf53
Ben Galloway <bdgalloway85@gmail.com>
2020-12-28 19:00:01 -0600

    add a note about morgan

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
+// morgan is used for server console logging
 app.use(morgan("dev"));
 
 // Requiring our routes
