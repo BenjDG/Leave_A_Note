db53f991967861995d6e1f207982c731ad2a585b
RaihanAkter03 <raihanakter26@gmail.com>
2021-01-02 13:42:36 -0600

    update profile handlebars

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,13 +1,10 @@
 
 $(document).ready(function () {
-
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function () {
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
-
     });
 
     // This file just does a GET request to figure out which user is logged in
