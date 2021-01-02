e2940dbb79267f0a7c336ff7e25ec167333ebd7e
Greg Stead <gregstead@gmail.com>
2021-01-02 11:55:52 -0600

    made get and post routes for note data

--- models/note.js
+++ models/note.js
@@ -1,28 +1,28 @@
-module.exports = function (sequelize, DataTypes) {
-  const Note = sequelize.define('Note', {
+module.exports = function(sequelize, DataTypes) {
+  const Note = sequelize.define("Note", {
     // Title cannot be empty, must be at least 1 char long
     title: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1]
-      }
+        len: [1],
+      },
     },
     body: {
       type: DataTypes.TEXT,
       allowNull: false,
       validate: {
-        len: [1]
-      }
-    }
+        len: [1],
+      },
+    },
   });
 
-  Note.associate = models => {
+  Note.associate = (models) => {
     Note.belongsTo(models.User, {
       foreignKey: {
         name: models.User.id,
-        allowNull: false
-      }
+        allowNull: false,
+      },
     });
   };
 
