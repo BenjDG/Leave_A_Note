94bf984eb1f7e09cdfc52fd2d844062903ca2958
Ben Galloway <bdgalloway85@gmail.com>
2021-01-02 13:13:24 -0600

    add jquery v1

--- public/js/notes.js
+++ public/js/notes.js
@@ -16,6 +16,33 @@ $(document).ready(() => {
     });
   });
 
+  // hydrate page
+
+  const $notesSection = $('#notes-section');
+  const $card = $('<div>').attr(
+    'class',
+    'tile is-4 is-child box is-flex is-flex-direction-column is-justify-content-space-between'
+  );
+  const $cardTitle = $('<div>').attr('class', 'title');
+  const $cardBody = $('<p>');
+  // const $cardBtnDelete;
+  // const $cardBtnEdit;
+  const $span = $('<span>').attr('class', 'icon is-medium');
+  const $iconSave = $('<i>').attr('class', 'far fa-save');
+  const $cardBtn = $('<button>').attr(
+    'class',
+    'button is-primary is-rounded is-large m-5 p-5 is-align-self-flex-end'
+  );
+  $notesSection.append($card);
+  $card.append($cardTitle);
+  $card.append($cardBody);
+  
+  $span.append($iconSave);
+  $
+  $card.append($cardBtn);
+
+  $notesSection.append($card);
+
   // hydrate page with note data
   $('.logout').on('click', function() {
     window.location.replace('/logout');
