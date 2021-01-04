21cf0ffad70818e32bed7f8d69d3e73aaafeda62
Ben Galloway <bdgalloway85@gmail.com>
2021-01-03 20:20:31 -0600

    cards with buttons layout v1

--- public/js/notes.js
+++ public/js/notes.js
@@ -13,38 +13,68 @@ $(document).ready(() => {
       console.log(element.id);
       console.log(element.title);
       console.log(element.body);
+      renderCardFactory();
     });
   });
 
   // hydrate page
-
+  // card section
   const $notesSection = $('#notes-section');
-  const $card = $('<div>').attr(
-    'class',
-    'tile is-4 is-child box is-flex is-flex-direction-column is-justify-content-space-between'
-  );
-  const $cardTitle = $('<div>').attr('class', 'title');
-  const $cardBody = $('<p>');
-  // const $cardBtnDelete;
-  // const $cardBtnEdit;
-  const $span = $('<span>').attr('class', 'icon is-medium');
-  const $iconSave = $('<i>').attr('class', 'far fa-save');
-  const $cardBtn = $('<button>').attr(
-    'class',
-    'button is-primary is-rounded is-large m-5 p-5 is-align-self-flex-end'
-  );
-  $notesSection.append($card);
-  $card.append($cardTitle);
-  $card.append($cardBody);
-  
-  $span.append($iconSave);
-  $
-  $card.append($cardBtn);
 
-  $notesSection.append($card);
+  // const $iconSave = $('<i>').attr('class', 'far fa-save');
+
+  function renderCardFactory () {
+    // individual cards
+    const $card = $('<div>').attr(
+      'class',
+      'tile is-4 is-child box is-flex is-flex-direction-column is-justify-content-space-between'
+    );
+    // card title
+    const $cardTitle = $('<p>').attr('class', 'title').text('JQuery');
+    // card body
+    const $cardBody = $('<p>').text('Some note about JQuery.');
+
+    // card buttons
+    // const $cardBtnDelete;
+    // const $cardBtnEdit;
+    // const $span = $('<span>').attr('class', 'icon is-medium');
+
+    // different buttons
+    // delete button
+
+    const $iconDelete = $('<i>').attr('class', 'far fa-trash-alt');
+    const $spanDel = $('<span>').attr('class', 'icon is-medium');
+    const $cardBtnDel = $('<button>').attr(
+      'class',
+      'button is-primary is-rounded is-large m-5 p-5 is-align-self-flex-end'
+    );
+    $spanDel.append($iconDelete);
+    $cardBtnDel.append($spanDel);
+
+    // update button
+    const $iconUpdate = $('<i>').attr('class', 'far fa-edit');
+    const $spanUpd = $('<span>').attr('class', 'icon is-medium');
+    const $cardBtnUpd = $('<button>').attr(
+      'class',
+      'button is-primary is-rounded is-large m-5 p-5 is-align-self-flex-end'
+    );
+    $spanUpd.append($iconUpdate);
+    $cardBtnUpd.append($spanUpd);
+
+    // attach title and body
+    $card.append($cardTitle);
+    $card.append($cardBody);
+
+    // attach buttons
+    $card.append($cardBtnUpd);
+    $card.append($cardBtnDel);
+
+    //attach card to section
+    $notesSection.append($card);
+  }
 
   // hydrate page with note data
-  $('.logout').on('click', function() {
+  $('.logout').on('click', function () {
     window.location.replace('/logout');
   });
 });
