7e19dfc7a3f60c14c07253d8ae8d6cc41722214a
Greg Stead <gregstead@gmail.com>
2021-01-04 17:55:28 -0600

    Endpoint returning JSON

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // Requiring our models and passport as we've configured it
-const db = require('../../models');
-const passport = require('../../config/passport');
+const db = require("../../models");
+const passport = require("../../config/passport");
 
-function loginCheck (req, res) {
+function loginCheck(req, res) {
   // If user is not logged in, send back an empty object
   if (!req.user) {
     return res.json({});
@@ -12,21 +12,21 @@ function loginCheck (req, res) {
 // Using the passport.authenticate middleware with our local strategy.
 // If the user has valid login credentials, send them to the members page.
 // Otherwise the user will be sent an error
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post("/login", passport.authenticate("local"), (req, res) => {
   // Sending back a password, even a hashed password, isn't a good idea
   res.json({
     email: req.user.email,
-    id: req.user.id
+    id: req.user.id,
   });
 });
 
 // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
 // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
 // otherwise send back an error
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   db.User.create(req.body)
     .then(() => {
-      res.redirect(307, '/api/login');
+      res.redirect(307, "/api/login");
     })
     .catch((err) => {
       res.status(401).json(err);
@@ -34,7 +34,7 @@ router.post('/signup', (req, res) => {
 });
 
 // Route for getting some data about our user to be used client side
-router.get('/user_data', (req, res) => {
+router.get("/user_data", (req, res) => {
   loginCheck(req, res);
   // Otherwise send back the user's email and id
   // Sending back a password, even a hashed password, isn't a good idea
@@ -43,21 +43,24 @@ router.get('/user_data', (req, res) => {
 });
 
 // Route for getting data about notes to be used client side
-router.route('/note_data')
+router
+  .route("/note_data")
   .get((req, res) => {
     loginCheck(req, res);
     const id = req.user.id;
     // Otherwise, send back the user's note
     db.Note.findAll({
       where: {
-        UserId: id
-      }
-    }).then(function (data) {
-      res.json(data);
-    }).catch(function (err) {
-      console.error(err);
-      res.send(err);
-    });
+        UserId: id,
+      },
+    })
+      .then(function(data) {
+        res.json(data);
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.send(err);
+      });
   })
   .post((req, res) => {
     loginCheck(req, res);
@@ -65,12 +68,26 @@ router.route('/note_data')
     db.Note.create({
       title: req.body.title,
       body: req.body.body,
-      UserId: req.user.id
-    }).then((d) => {
-      res.json(d);
-    }).catch(function (err) {
+      UserId: req.user.id,
+    })
+      .then((d) => {
+        res.json(d);
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.send(err);
+      });
+  });
+// Handler for api/view_group_choices
+router.get("/view_group_choices", (req, res) => {
+  // Get all groups
+  db.Group.findAll()
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(function(err) {
       console.error(err);
       res.send(err);
     });
-  });
+});
 module.exports = router;
